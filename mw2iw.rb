33b566aecc573eb9cffcc0190f9a6b5f411773d1
docunext <albert.lash@docunext.com>
2010-04-28 20:23:15 -0400

    revisions working

--- mw2iw.rb
+++ mw2iw.rb
@@ -3,10 +3,15 @@ require 'mydbsetup'
 dbh = DBI.connect(@mydb[:host], @mydb[:user], @mydb[:pass])
 
 
-sth = dbh.prepare("SELECT hungrywiki_text.old_text
-FROM  hungrywiki_text, hungrywiki_page
-WHERE hungrywiki_text.old_id = hungrywiki_page.page_latest
+sth = dbh.prepare("SELECT hungrywiki_revision.rev_id
+FROM  hungrywiki_revision, hungrywiki_page
+WHERE hungrywiki_revision.rev_page = hungrywiki_page.page_id
 AND hungrywiki_page.page_title = ?")
+
+sthz = dbh.prepare("SELECT hungrywiki_text.old_text
+FROM  hungrywiki_text
+WHERE hungrywiki_text.old_id = ?")
+
 File.open('page_index.mdwn') do |f|
   f.each_line do |page|
     sth.execute(page.gsub("\n",''))
@@ -14,6 +19,16 @@ File.open('page_index.mdwn') do |f|
       results.each do |row|
         begin
           puts row[0]
+          sthz.execute(row[0])
+          if res2 = sthz.fetch_all
+            res2.each do |row|
+              begin
+                puts row[0]
+              rescue NoMethodError
+                puts row.inspect
+              end
+            end
+          end
         rescue NoMethodError
           puts row.inspect
         end
